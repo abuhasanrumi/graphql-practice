8b5ee22cd3f8b3e3846f87c21a854f53e8f9b215
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-06-02 17:30:35 +0600

    mutation adding

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -11,5 +11,39 @@ exports.Mutation = {
 
         categories.push(newCategory)
         return newCategory
+    },
+
+    addProduct: (parent, { input }, { products, categories }) => {
+        const { name, description, quantity, price, image, onSale, categoryId } = input;
+
+        const newProduct = {
+            id: uuid(),
+            name,
+            description,
+            quantity,
+            price,
+            image,
+            onSale,
+            categoryId,
+        }
+
+        products.push(newProduct)
+        return newProduct
+    }
+    ,
+
+    addReview: (parent, { input }, { reviews }) => {
+        const { date, title, rating, productId, } = input;
+
+        const newReview = {
+            id: uuid(),
+            date,
+            title,
+            rating,
+            productId
+        }
+
+        reviews.push(newReview)
+        return newReview
     }
 }
